87b88745f1dc0776931321c70f0fa88852645869
zaki-coder <zakiazizi1841992@gmail.com>
2022-09-14 11:42:45 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Content from './components/Content';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="">
       <NavBar />
       <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex space-x-12 justify-end pr-8 text-gray-400">
+      <p className="mt-14">Datenschutz</p>
+      <p className="mt-14">Impressum</p>
+    </div>
+  );
+}
+
+export default Footer
